57536f944a1e49fa2c3465e8b5af18cc9af6af4c
senup <460705520@qq.com>
2019-10-19 19:25:36 +0800

    添加分页

--- src/main/java/com/dbis/asset/controller/AssetController.java
+++ src/main/java/com/dbis/asset/controller/AssetController.java
@@ -2,6 +2,8 @@ package com.dbis.asset.controller;
 
 import com.dbis.asset.mapper.AssetMapper;
 import com.dbis.asset.pojo.Asset;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,30 +24,51 @@ public class AssetController {
     @Autowired
     private AssetMapper assetMapper;
 
-    //查找所有资产，并携带类别
+
+
+    /**
+     * 功能描述: //查找所有资产，并携带类别<br>
+     * 〈〉
+     * @Param: [pageNum, pageSize]
+     * @Return: java.util.Map<java.lang.String,java.lang.Object>
+     * @Author: tom
+     * @Date: 2019/10/19 19:11
+     */
     @GetMapping
-    public HashMap<String,Object> findAll(){
-       List<Asset> asset = assetMapper.selectAllWithCategory();
-        HashMap<String, Object> hashMap = new HashMap<>();
-        hashMap.put("data",asset);
-        hashMap.put("status",1);
-        return hashMap;
+    public Map<String, Object> findAll(
+            @RequestParam(defaultValue = "1",value = "pageNum") Integer pageNum,
+            @RequestParam(defaultValue = "5",value = "pageSize") Integer pageSize
+                                           ){
+        PageHelper.startPage(pageNum,pageSize);
+        List<Asset> list = assetMapper.selectAllWithCategory();
+        PageInfo<Asset> info = new PageInfo<>(list);
+        Map<String, Object> map = new HashMap<>();
+        map.put("data",info);
+        map.put("status",1);
+        return map;
     }
+
+
 
     //模糊搜索
     @GetMapping("/find")
-    public HashMap<String,Object> findByLike(@ModelAttribute Asset asset){
-        List<Asset> data = assetMapper.selectByLike(asset);
-        HashMap<String, Object> hashMap = new HashMap<>();
-        hashMap.put("data",data);
-        hashMap.put("status",1);
-        return hashMap;
+    public Map<String,Object> findByLike(@ModelAttribute Asset asset,
+                                         @RequestParam(defaultValue = "1",value = "pageNum") Integer pageNum,
+                                         @RequestParam(defaultValue = "5",value = "pageSize") Integer pageSize
+                                         ){
+        PageHelper.startPage(pageNum,pageSize);
+        List<Asset> list = assetMapper.selectByLike(asset);
+        PageInfo<Asset> info = new PageInfo<>(list);
+        Map<String, Object> map = new HashMap<>();
+        map.put("data",info);
+        map.put("status",1);
+        return map;
     }
 
     //添加资产
     @PostMapping
-    public HashMap<String,Object> addAsset(@ModelAttribute Asset asset){
-        HashMap<String, Object> map = new HashMap<>();
+    public Map<String,Object> addAsset(@ModelAttribute Asset asset){
+        Map<String, Object> map = new HashMap<>();
         if(assetMapper.insert(asset)!=0){
             map.put("status",1);
             return map;
---
+++ src/main/java/com/dbis/asset/controller/CategoryController.java
@@ -0,0 +1,17 @@
+package com.dbis.asset.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName CategoryController
+ * @Description TODO
+ * @Author tom
+ * @Date 2019/10/19 19:07
+ * @Version 1.0
+ **/
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+}
