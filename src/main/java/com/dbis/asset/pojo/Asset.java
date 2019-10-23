73c904bd978632d982a2d89a74009b5e124b9c58
senup <460705520@qq.com>
2019-10-23 14:58:29 +0800

    修复修改时间modifytime格式不正确

--- src/main/java/com/dbis/asset/pojo/Asset.java
+++ src/main/java/com/dbis/asset/pojo/Asset.java
@@ -24,6 +24,8 @@ public class Asset {
 
     private Integer status;//状态
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date modifyTime;//修改时间，自动添加
 
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
